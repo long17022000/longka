5bf34d1d5136ec69bb9d72327ca3af6aa658eb63
long17022000 <vietlong17022000@gmail.com>
2021-02-07 14:32:17 +0700

    edit sp.php

--- sp.php
+++ sp.php
@@ -5,12 +5,18 @@
 <meta charset="utf-8">
 <title>Untitled Document</title>
 <style>
+	.img{width: 140px; height: 160px; float: left; margin-left: 5px; margin-top:10px; text-align:center}
+	.menu{background: #ccc; font-size: 20px; text-align: right; padding-right: 100px;}
+	 a {text-decoration: none;} 
+	 .text{ font-weight:bold; font-size:30px; font-family:Georgia, "Times New Roman", Times, serif; text-align:center; padding-top:10px; height:50px}
 </style>
 </head>
 <body>
 
 	<div class="menu">
 		<div class="text">Quản lí sản phẩm</div>
+	<a href="file:///C|/xampp/htdocs/long/web/form.php">Thêm mới</a>
+	<a href="file:///C|/xampp/htdocs/long/web/list.php">Thoát</a>
 	</div>
 
 	<?php
